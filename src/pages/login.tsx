a148a4a825e12f5873efef3286a57c9e47f41f5d
beingimran <imranben35@gmail.com>
2023-09-13 20:59:30 +0530

    last commit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -184,7 +184,7 @@ export default function Login() {
                 </svg>
                 <span className="sr-only">Fire icon</span>
               </div>
-              <div className="ml-3 text-sm font-normal">User Don't exist</div>
+              <div className="ml-3 text-sm font-normal">User Dont exist</div>
               <button
                 type="button"
                 className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
